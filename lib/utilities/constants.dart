2267a3b9b879a8ca864cf0cf4a3b1d40a1e0abe6
Bryan Delgado <bryand9970@gmail.com>
2024-06-29 20:26:23 -0500

    Sprint 1: Mejora en el apartado de recuperación de contraseña.

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: constant_identifier_names
 
-const backend_URL =  "https://backendtesis.onrender.com/api/";
+//const backend_URL =  "https://backendtesis.onrender.com/api/";
+
+const backend_URL = "https://backend-tesis.up.railway.app/api/";
+
 const login = "${backend_URL}cliente/login";
 const perfilCliente = "${backend_URL}cliente";
 const proforma = "$backend_URL/proformas/orden";
